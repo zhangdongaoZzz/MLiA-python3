5014a92da1926cf8a398321197c039773bbf2d21
zhangdongaoZzz <38176633+zhangdongaoZzz@users.noreply.github.com>
2019-04-29 19:44:58 +0800

    Update and rename demo-2.1-2.2.py to demo.py

--- CH02/demo.py
+++ CH02/demo.py
@@ -4,7 +4,7 @@ import  numpy as  np
 from pylab import  *
 
 
-
+#2.2
 group,labels=KNN.creatDataSet()
 
 datingDataMat,datingLabels=KNN.file2matrix(r'C:\Users\zhang\Desktop\ml\machinelearninginaction\Ch02\datingTestSet2.txt')
@@ -14,3 +14,7 @@ KNN.plot_KNN(datingDataMat,datingLabels,0,2)
 print(KNN.autoNorm(datingDataMat))
 
 KNN.datingClassTest(r'C:\Users\zhang\Desktop\ml\machinelearninginaction\Ch02\datingTestSet2.txt')
+
+#2.3
+c=KNN2.img2vector(r'C:\Users\zhang\Desktop\ml\machinelearninginaction\Ch02\trainingDigits\0_0.txt')
+KNN2.handwritingClassTest()
