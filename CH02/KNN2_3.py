87bfdd4c9bcfe9b492338e7d7ae44d83b7060bdb
zhangdongaoZzz <38176633+zhangdongaoZzz@users.noreply.github.com>
2019-04-29 19:44:20 +0800

    Create KNN2_3.py
    识别字体

---
+++ CH02/KNN2_3.py
@@ -0,0 +1,40 @@
+import numpy as np
+import  operator
+from pylab import  *
+import matplotlib.pyplot as plt
+from  functools import  reduce
+import  os
+import KNN
+
+def img2vector(filename):
+    vector=np.zeros((1,1024))
+    fr=open(filename)
+    for i in range(32):
+        line=fr.readline()
+        for j in range(32):
+            vector[0,32*i+j]=int(line[j])
+    return vector
+
+def handwritingClassTest(trainpath=r'C:\Users\zhang\Desktop\ml\machinelearninginaction\Ch02\trainingDigits',testpath=r'C:\Users\zhang\Desktop\ml\machinelearninginaction\Ch02\testDigits'):
+    hwLabels=[]
+    trainingFileList=os.listdir(trainpath)
+    m=len(trainingFileList)
+    trainMat=np.zeros((m,1024))
+    for i in range(m):
+        fileNameStr=trainingFileList[i]
+        label=int(fileNameStr.split('_')[0])
+        hwLabels.append(label)
+        trainMat[i,:]=img2vector('{}/{:s}'.format(trainpath,fileNameStr))
+    testFileList=os.listdir(testpath)
+    error=0.0
+    mTest=len(testFileList)
+    for i in range(mTest):
+        fileNameStr=testFileList[i]
+        classNumStr=int(fileNameStr.split('_')[0])
+        vectorUnderTest=img2vector('{}/{:s}'.format(testpath,fileNameStr))
+        classifierResult=KNN.classify0(vectorUnderTest,trainMat,hwLabels,3)
+        print('the pre:%d,  the real:%d'%(classifierResult,classNumStr))
+        if classifierResult!=classNumStr:
+            error+=1
+    print('error rate:%f'%(error/mTest))
+
